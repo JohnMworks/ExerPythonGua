627269221ef56239d5532345201a91d46d337b06
johney <168041510+juninndz@users.noreply.github.com>
2026-07-14 19:39:16 -0300

    novos ex

--- ex/ex017.py
+++ ex/ex017.py
@@ -3,7 +3,7 @@ co = float(input('digite o cateto oposto'))
 ca = float(input('digite o cateto adjacente'))
 math.
 print('A hipotenusa eh {}'.format())
-
+a
 '''from math import sqrt
 co = float(input('digite o cateto oposto: '))
 ca = float(input('digite o cateto adjacente'))
