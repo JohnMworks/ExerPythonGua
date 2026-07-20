ac961f8d53ceef98297df63719a035cf44cc6bc2
johney <168041510+juninndz@users.noreply.github.com>
2026-07-19 22:42:47 -0300

    novos ex

--- ex/ex020.py
+++ ex/ex020.py
@@ -1,7 +1,7 @@
 import random
 no1 = str(input('Digite o primeiro nome: '))
-no2 = str(input('Digite o segundo nome'))
-no3 = str(input('Digite o terceiro nome'))
+no2 = str(input('Digite o segundo nome: '))
+no3 = str(input('Digite o terceiro nome: '))
 
 lista = [no1,no2,no3]
 random.shuffle(lista)
