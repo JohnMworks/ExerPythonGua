d7eeb8d4add45f7dfc0165f503fcf3f14d22f920
johney <168041510+juninndz@users.noreply.github.com>
2026-07-24 16:27:50 -0300

    novos ex

--- ex/ex021.py
+++ ex/ex021.py
@@ -1,1 +1,8 @@
-print('musica')
+import music21
+import pygame
+pygame.init()
+pygame.mixer.music.load(music21.mp3)
+pygame.mixer.music.play()
+pygame.event.wait()
+
+#pygame n tem suporte p python atual :(
