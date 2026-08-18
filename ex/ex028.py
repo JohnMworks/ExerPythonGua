018aebb3425801029a4077b6861daf8288e6a836
johney <168041510+juninndz@users.noreply.github.com>
2026-08-18 12:30:05 -0300

    novos ex

--- ex/ex028.py
+++ ex/ex028.py
@@ -7,4 +7,4 @@ num = int(input('Vou pensar em um numero entre 0 e 5, tente adivinhar: '))
 if num == a:
     print('Parabens, eu realmente pensei no {}'.format(num))
 else:
-    print('Errou! eu pensei no {}, não no {}'.format(a,num))
+    print('Errou! eu pensei no {}, não no {}'.format(a,num ))
