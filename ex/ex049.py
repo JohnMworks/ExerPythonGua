b2c9b5453ae64dc66f75a958f7e997f3de46af5f
johney <168041510+juninndz@users.noreply.github.com>
2026-08-24 20:57:38 -0300

    feat(python): exercício 49 adicionado.

--- ex/ex049.py
+++ ex/ex049.py
@@ -1,2 +1,7 @@
-num = int(input("Digite um número:"))
+num = int(input('Digite um número para receber sua tabuada: '))
 
+print("=+" * 8)
+for i in range (11):
+    res = num *i
+    print(f'   {num} x {i} = {res}')
+print("=+" * 8)
