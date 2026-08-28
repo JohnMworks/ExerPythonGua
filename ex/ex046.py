76d1eae9eb725634bf2fc7f6572120ab8c8299ff
johney <168041510+juninndz@users.noreply.github.com>
2026-08-28 08:39:43 -0300

    chore(python):revisão de exer ja feitos

--- ex/ex046.py
+++ ex/ex046.py
@@ -2,7 +2,7 @@ import time
 
 contagem = int(input('Digite um número para iniciar a contagem: '))
 
-for i in range(contagem, 0, -1):
+for i in range(contagem, -1, -1):
     time.sleep(1)
     print(i)
 print('Boom!')
--- ex/ex047.py
+++ ex/ex047.py
@@ -1,7 +1,18 @@
  #numeros pares entre 1 e 50
  
- num = int(input('Digite um número: '))
+ #num = int(input('Digite um número: '))
  
- for i in range(1, 50):
+num = 50
+ 
+
+for i in range(0,51, 2):
+    print(i)
+print('Acabou')
+ 
+ 
+ 
+ 
+ #roda o laço duas vezes (uma pra impares n impressos e outra p pares q sao imprimidos)
+'''  for i in range(1, 50):
      if i % 2 == 0:
-         print(i)
+         print(i) '''
